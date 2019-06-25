34cdaf7e88dcf6d3ccf00777b91357c4199566f8
Omar I. Martínez Gómez <omar_238@hotmail.com>
2019-06-25 12:25:18 -0500

    ejercicio completo

---
+++ ProbadorSubprocesos/ImpresoraMensajes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProbadorSubprocesos
+{
+    class ImpresoraMensajes
+    {
+        #region Campos Privados
+        private int tiempoInactividad;
+        private static Random aleatorio = new Random();
+        #endregion
+
+
+
+        #region Constructor
+        public ImpresoraMensajes()
+        {
+            // Elije tiempo de inactividad aleatorio entre 0 y 5 segundos (5001 milisegundos)
+            tiempoInactividad = aleatorio.Next(5001);
+        }
+        #endregion
+
+
+
+        #region Métodos Públicos
+        public void Imprimir()
+        {
+            // Obtiene la referencia al subproceso que se ejecuta en este momento.
+            Thread actual = Thread.CurrentThread;
+            Console.WriteLine($"{actual.Name} va a estar inactivo durante {tiempoInactividad} milisegundos");
+
+            // Inactivo durante tiempoInactividad milisegundos
+            Thread.Sleep(tiempoInactividad);
+
+            // Imprime el nombre del subproceso
+            Console.WriteLine($"{actual.Name} dejó de estar inactivo");
+        }
+        #endregion
+    }
+}
--- ProbadorSubprocesos/Program.cs
+++ ProbadorSubprocesos/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProbadorSubprocesos
@@ -10,6 +11,28 @@ namespace ProbadorSubprocesos
     {
         static void Main(string[] args)
         {
+            // Crea y asigna un nombre a cada subproceso. usa el método Imprimir de ImpresoraMensajes comoa rgumento para el delegado ThreadStart.
+            ImpresoraMensajes impresora1 = new ImpresoraMensajes();
+            Thread subproceso1 = new Thread(new ThreadStart(impresora1.Imprimir));
+            subproceso1.Name = "subproceso1";
+
+            ImpresoraMensajes impresora2 = new ImpresoraMensajes();
+            Thread subproceso2 = new Thread(new ThreadStart(impresora2.Imprimir));
+            subproceso2.Name = "subproceso2";
+
+            ImpresoraMensajes impresora3 = new ImpresoraMensajes();
+            Thread subproceso3 = new Thread(new ThreadStart(impresora3.Imprimir));
+            subproceso3.Name = "subproceso3";
+
+            Console.WriteLine("Iniciando Subprocesos");
+
+            // Llama al método Start de cada subproceso para colocar a cada uno de ellos en el estado Running
+            subproceso1.Start();
+            subproceso2.Start();
+            subproceso3.Start();
+
+            Console.WriteLine("Subprocesos iniciados\n");
+            Console.ReadLine();
         }
     }
 }
